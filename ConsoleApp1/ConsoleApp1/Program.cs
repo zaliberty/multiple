4caeac7ddf05732e3668b05bcb964d4836ab3ccd
zaliberty <dream_vital@mail.ru>
2019-04-05 17:18:33 +0300

    init commit

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double drobnoeChislo = 10.10;
+            Write(drobnoeChislo);
+            int summa = Multiple(25, 55);
+            Write(summa);
+
+           Console.ReadKey();
+        }
+
+        static void Write(int x)
+        {
+            Console.WriteLine(x);
+        }
+
+        static void Write(double x)
+        {
+            Console.WriteLine(x);
+        }
+
+
+        static int Multiple(int x, int y)
+        {
+            int summa = x * y;
+            return summa;
+        }
+    }
+}
